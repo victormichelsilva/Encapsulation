f5294c8ccb5446632b8424bfef7629b1483598a6
victormichelsilva <victormichelsilva@gmail.com>
2021-11-23 10:07:38 -0300

    Adding files

---
+++ Conta.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulamento1
+{
+    class Conta
+    {
+        private decimal saldo;
+
+        public decimal Saldo 
+        {
+            get { return saldo; }
+
+            set 
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("O saldo não pode ser negativo");
+                }
+                else 
+                {
+                    saldo = value;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Encapsulamento1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var conta1 = new Conta();
+                conta1.Saldo = -10;
+
+                Console.WriteLine($"O saldo da conta é {conta1.Saldo}");
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
